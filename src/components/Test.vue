fcb55c69e6520cd39bbe707ce744d7cb66bca32f
Markus Appel <markospoparkos@gmail.com>
2021-11-28 22:49:03 +0100

    implement hook and test component

---
+++ src/components/Test.vue
@@ -0,0 +1,127 @@
+<template>
+  <h1>Test Pagination</h1>
+
+  <form action="" @submit.prevent.stop>
+    <label>
+      num buttons
+      <input type="number" v-model.number="numButtons">
+    </label>
+    <br>
+    <label>
+      num items
+      <input type="number" v-model.number="numItems">
+    </label>
+    <br>
+    <label>
+      page size
+      <select v-model.number="pageSize">
+        <option>5</option>
+        <option>10</option>
+        <option>25</option>
+        <option>50</option>
+        <option>100</option>
+      </select>
+    </label>
+    <br>
+    <label>
+      page
+      <input type="number" v-model.number="page">
+    </label>
+    <br>
+    <div>num pages: {{ numPages }}</div>
+
+    <br>
+
+    <div>has prev: {{ hasPrev }}</div>
+    <div>has next: {{ hasNext }}</div>
+
+    <br>
+
+    <div>slice: {{ slice }}</div>
+
+    <br>
+
+    <label>buttons:</label>
+    <ul>
+      <li v-for="(button, idx) in buttons" :key="idx">
+        {{ button }}
+      </li>
+    </ul>
+
+    <label>styled:</label>
+    <ul class="paginator">
+      <li class="control" :class="!hasPrev && 'disabled'" @click="goStart">
+        {{ '<<' }}
+      </li>
+      <li class="control" :class="!hasPrev && 'disabled'" @click="goPrev">
+        {{ '<' }}
+      </li>
+      <li
+        v-for="(button, idx) in buttons"
+        :key="idx"
+        :class="{ 'current-page': button.active }"
+        @click="page = button.page"
+      >
+        {{ button.ellipsis ? '...' : button.page }}
+      </li>
+      <li class="control" :class="!hasNext && 'disabled'" @click="goNext">
+        {{ '>' }}
+      </li>
+      <li class="control" :class="!hasNext && 'disabled'" @click="goEnd">
+        {{ '>>' }}
+      </li>
+    </ul>
+
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import usePagination from '../usePagination'
+
+export default defineComponent({
+  setup() {
+    const numButtons = ref(2)
+    const pagination = usePagination(numButtons)
+    return {
+      ...pagination,
+      numButtons,
+    }
+  }
+})
+</script>
+
+<style scoped>
+ul.paginator {
+  display: flex;
+  list-style: none;
+  user-select: none;
+}
+ul.paginator li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  width: 50px;
+  height: 50px;
+  border: 1px solid black;
+}
+ul.paginator li:not(.current-page):hover {
+  font-weight: bold;
+  border-width: 2px;
+  cursor: pointer;
+}
+ul.paginator li.current-page {
+  color: blue;
+  border-color: blue;
+}
+ul.paginator li.control {
+  border: none;
+  font-weight: 900;
+  cursor: pointer;
+}
+ul.paginator li.control.disabled {
+  opacity: 0.5;
+  pointer-events: none;
+}
+</style>
---
+++ src/usePagination.ts
@@ -0,0 +1,188 @@
+import { ref, computed, watch, Ref, ComputedRef } from 'vue'
+
+export interface IPagination {
+  page: Ref<number>
+  pageSize: Ref<number>
+  numItems: Ref<number>
+  numPages: ComputedRef<number>
+  slice: ComputedRef<[number, number]>
+  buttons: ComputedRef<IPaginatorButton[]>
+  hasPrev: ComputedRef<boolean>
+  hasNext: ComputedRef<boolean>
+  goPrev: () => number
+  goNext: () => number
+  goStart: () => 1
+  goEnd: () => number
+}
+
+interface IButton {
+  page: number
+}
+
+export interface IPageButton extends IButton {
+  active: boolean
+  ellipsis: false
+}
+
+export interface IEllipsisButton extends IButton {
+  active: false
+  ellipsis: true
+}
+
+export type IPaginatorButton = IPageButton | IEllipsisButton
+
+
+export default (numButtons: Ref<number> = ref(5)): IPagination => {
+  const pageSize = ref(5)
+  const numItems = ref(0)
+
+  const _page = ref(1)
+  const page = computed({
+    get: () => _page.value,
+    set: val => {
+      if (val > 0 && val <= numPages.value) {
+        _page.value = val
+      }
+    }
+  })
+
+  const numPages = computed(() => Math.ceil(numItems.value / pageSize.value))
+
+  watch([pageSize, numItems], () => {
+    if (page.value > numPages.value) {
+      page.value = numPages.value
+    }
+  })
+
+  const slice = computed((): [number, number] => {
+    const start = (page.value - 1) * pageSize.value
+    const end = Math.min(start + pageSize.value, numItems.value)
+    return [start, end]
+  })
+
+  const hasPrev = computed(() => page.value > 1)
+  const hasNext = computed(() => page.value < numPages.value)
+
+  const goPrev = () => {
+    if (hasPrev.value) [
+      page.value--
+    ]
+    return page.value
+  }
+  const goNext = () => {
+    if (hasNext.value) {
+      page.value++
+    }
+    return page.value
+  }
+  const goStart = (): 1 => {
+    page.value = 1
+    return 1
+  }
+  const goEnd = () => {
+    page.value = numPages.value
+    return numPages.value
+  }
+
+  const buttons = computed<Array<IPaginatorButton>>(() => {
+    if (numPages.value <= numButtons.value) {
+      return fullButtons(page.value, numPages.value)
+    } else {
+      return partialButtons(page.value, numPages.value, numButtons.value)
+    }
+  })
+
+  return {
+    page,
+    pageSize,
+    numItems,
+    numPages,
+    slice,
+    hasPrev,
+    hasNext,
+    goPrev,
+    goNext,
+    goStart,
+    goEnd,
+    buttons,
+  }
+}
+
+
+function fullButtons(page: number, numPages: number): Array<IPaginatorButton> {
+  return consecutiveSlots(1, page)
+    .concat({ page, active: true, ellipsis: false })
+    .concat(consecutiveSlots(page + 1, numPages + 1))
+}
+
+
+function partialButtons(
+  page: number, numPages: number, numButtons: number
+): Array<IPaginatorButton> {
+  let slotsLeft = 0, slotsRight = 0
+  const halvedButtons = Math.floor(numButtons / 2)
+
+  // check if either side can be filled with pages up to active page
+  if (page <= halvedButtons) {
+    slotsLeft = page - 1
+    slotsRight = numButtons - slotsLeft - 1
+
+  } else if (page >= numPages - halvedButtons) {
+    slotsRight = numPages - page
+    slotsLeft = numButtons - slotsRight - 1
+
+  // page sits in center and has 1 or 2 adjacent ellipsis
+  } else if (numButtons % 2 === 0) {
+    slotsLeft = halvedButtons - 1
+    slotsRight = halvedButtons
+  } else {
+    slotsLeft = halvedButtons
+    slotsRight = halvedButtons
+  }
+
+  const pagesPrev = page - 1
+  const pagesNext = numPages - page
+
+  let list: Array<IPaginatorButton> = []
+
+  if (pagesPrev <= slotsLeft) {
+    list = consecutiveSlots(1, pagesPrev + 1)
+
+  } else if (slotsLeft > 0) {
+    const ellipsis = slotsLeft === 2 ? 2 : Math.ceil(slotsLeft / 2)
+    const remaining = slotsLeft - ellipsis
+    let ellipsisPage = Math.ceil(pagesPrev / 2)
+    if (pagesPrev % 2 === 0) {
+      ellipsisPage++
+    }
+
+    list = (consecutiveSlots(1, ellipsis) as Array<IPaginatorButton>)
+      .concat({ page: ellipsisPage, active: false, ellipsis: true })
+      .concat(consecutiveSlots(page - remaining, page))
+  }
+
+  list.push({ page, active: true, ellipsis: false })
+
+  if (pagesNext <= slotsRight) {
+    list = list.concat(consecutiveSlots(page + 1, numPages + 1))
+
+  } else if (slotsRight > 0) {
+    const ellipsis = slotsRight === 2 ? 1 : Math.floor(slotsRight / 2) + 1
+    const remaining = slotsRight - ellipsis
+    const ellipsisPage = page + Math.ceil(pagesNext / 2)
+
+    list = list.concat(consecutiveSlots(page + 1, page + ellipsis))
+      .concat({ page: ellipsisPage, active: false, ellipsis: true })
+      .concat(consecutiveSlots(numPages - remaining + 1, numPages + 1))
+  }
+
+  return list
+}
+
+function consecutiveSlots(startIdx: number, endIdx: number): Array<IPageButton> {
+  const result: Array<IPageButton> = []
+  for (let idx = startIdx; idx < endIdx; idx++) {
+    result.push({ page: idx, active: false, ellipsis: false })
+  }
+  return result
+}
